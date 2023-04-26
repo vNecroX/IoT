1cd143c26601ab78ed415191b31a9aecfee101b3
darkঐু <a16100266@live.ceti.mx>
2023-04-25 21:13:54 -0600

    Arduino

--- Arduino/Sound_Sensor_KY-038/Sound_Sensor_KY-038.ino
+++ Arduino/Sound_Sensor_KY-038/Sound_Sensor_KY-038.ino
@@ -4,7 +4,7 @@ const int soundRead = A1;
 float randomN;
 
 float soundLevel;
-const float threshold = 64.45;
+const float threshold = 64.5;
 
 void setup(){
   pinMode(LED, OUTPUT);
@@ -18,16 +18,16 @@ void loop(){
   randomN /= 100;
   Serial.print("Random: ");
   Serial.println(randomN);
-  
+    
   soundLevel = analogRead(soundRead);
   soundLevel += randomN;
   Serial.print("Sound level: ");
   Serial.println(soundLevel);
   Serial.print("\n");
 
-  // Our limit is 64.45, if sound level is equal or exceed it, then, based on the random number
-  // there is a reduced chance to get and 45 or higher number up to 60, in other words
-  // 1/4 or 0.25 chances to turn on the LED as a detected sound
+  // Our limit is 64.5, if sound level is equal or exceed it, then, based on the random number
+  // there is a reduced chance to get and 50 or higher number up to 60, in other words
+  // 1/6 or 0.16 chances to turn on the LED as a detected sound
   if(soundLevel >= threshold) digitalWrite(LED, HIGH);
   else digitalWrite(LED, LOW);
 
